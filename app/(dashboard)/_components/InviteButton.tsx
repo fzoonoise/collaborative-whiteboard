b5de2a1f6607179007673b673826b4d5e834d7b4
ff <ffworkbench@gmail.com>
2025-04-09 00:39:12 +0800

    fix: radix ui a11y issue

--- app/(dashboard)/_components/InviteButton.tsx
+++ app/(dashboard)/_components/InviteButton.tsx
@@ -2,7 +2,7 @@ import { OrganizationProfile } from "@clerk/nextjs";
 import { Plus } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import { Dialog, DialogContent, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
 
 export const InviteButton = () => {
   return (
@@ -15,6 +15,7 @@ export const InviteButton = () => {
       </DialogTrigger>
 
       <DialogContent className="p-0 bg-transparent border-none max-w-[880px]">
+      <DialogTitle className="sr-only">Organization Settings</DialogTitle>
         <OrganizationProfile />
       </DialogContent>
     </Dialog>
