355786f84a8d91fb3244f2f9f1be3782ed1c2fdc
ff <ffworkbench@gmail.com>
2025-04-11 23:27:20 +0800

    feat: add Participants component and integrate Shadcn Avatar

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,18 +5,36 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+const UserColors = [
+  "#006400",
+  "#ADFF2F",
+  "#808080",
+  "#E9967A",
+  "#FFDEAD",
+  "#00BFFF",
+  "#FF4500",
+  "#008080",
+  "#808000",
+  "#9ACD32",
+];
+
+export function connectionIdToColor(connectionId: number): string {
+  // The connectionId is typically an incrementing integer assigned by the server.
+  return UserColors[connectionId % UserColors.length];
+}
+
 export function devLog(name: string, value: unknown): void {
-  if (process.env.NODE_ENV !== 'development') return;
+  if (process.env.NODE_ENV !== "development") return;
 
   let output: string;
 
-  if (typeof value === 'function') {
-    output = '[Function: cannot stringify]';
+  if (typeof value === "function") {
+    output = "[Function: cannot stringify]";
   } else {
     try {
       output = JSON.stringify(value, null, 2);
     } catch {
-      output = '[Unserializable value]';
+      output = "[Unserializable value]";
     }
   }
 
