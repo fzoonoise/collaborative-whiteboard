3f93d0ad784381c100d2487a196f37cb281fbfe9
ff <ffworkbench@gmail.com>
2025-04-14 21:17:41 +0800

    revert: move Canvas.tsx and Info.tsx back to original location

--- app/board/[boardId]/_components/Loading.tsx
+++ app/board/[boardId]/_components/Loading.tsx
@@ -1,6 +1,6 @@
 import { Loader } from "lucide-react";
 
-import Info from "./Info";
+import Info from "./Canvas/Info";
 import Participants from "./Participants/Participants";
 import Toolbar from "./Toolbar/Toolbar";
 
