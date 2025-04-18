d555db16f21baf194b577691c5adebc914bcf5fc
ff <ffworkbench@gmail.com>
2025-04-18 23:10:35 +0800

    feat: update document title on board name edit & Loading style

--- app/board/[boardId]/_components/Loading.tsx
+++ app/board/[boardId]/_components/Loading.tsx
@@ -10,7 +10,7 @@ export const Loading = () => {
       className="h-full w-full relative bg-neutral-100 touch-none
     flex items-center justify-center"
     >
-      <Loader className="h-6 w-6 text-muted-foreground animate-spin" />
+      <Loader className="h-8 w-8 text-muted-foreground animate-spin" />
       <Info.Skeleton />
       <Participants.Skeleton />
       <Toolbar.Skeleton />
--- app/board/[boardId]/page.tsx
+++ app/board/[boardId]/page.tsx
@@ -1,19 +1,27 @@
 "use client";
 
 import { useEffect } from "react";
+import { useQuery } from "convex/react";
 
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+
+import { Loading } from "./_components/Loading";
 import { Room } from "@/components/Room";
 import Canvas from "./_components/Canvas/Canvas";
-import { Loading } from "./_components/Loading";
 
 type BoardIdPageProps = {
   params: { boardId: string };
 };
 
 const BoardIdPage = ({ params }: BoardIdPageProps) => {
+  const data = useQuery(api.board.getBoardById, {
+    id: params.boardId as Id<"boards">,
+  });
+
   useEffect(() => {
-    document.title = `Whiteboard`;
-  }, []);
+    document.title = data?.title || "New board";
+  }, [data?.title]);
 
   return (
     <Room roomId={params.boardId} fallback={<Loading />}>
